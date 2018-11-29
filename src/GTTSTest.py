1c98002194738ab4b4358f27f49cdddf67fdd99e
Dylan Hoffmann <dylan.c.hoffmann@gmail.com>
2018-11-29 00:02:26 -0500

    Parsing text for pauses fixed

--- src/GTTSTest.py
+++ src/GTTSTest.py
@@ -1,13 +1,21 @@
 from playsound import playsound
+from playsound import playsound
 
 #Iterate over the line and insert breaks at each .,: for realistic speech
 def parseText(text):
-    for word in text:
-        for char in word:
-            if char == ".":
-                pass
-    ssml = "<speak>" + text + "</speak>"
-    return ssml
+    line = ""
+    for word in text.split():
+        print("word", word)
+        if word[-1] == ".":
+            word = word + "<break time=\"700ms\" />"
+        elif word[-1] == ",":
+            word = word + "<break time=\"450ms\" />"
+        elif word[-1] == ":":
+            word = word + "<break time=\"250ms\" />"
+        word += " "
+        line+= word
+    print("text", text, "line", line)
+    return line
 
 def synthesize_ssml():
     """Synthesizes speech from the input string of ssml.
@@ -19,15 +27,18 @@ def synthesize_ssml():
     """
     from google.cloud import texttospeech
     client = texttospeech.TextToSpeechClient()
-
-    text = "To be, or not to be, that is the question: " \
-           "Whether 'tis nobler in the mind to suffer " \
-           "The slings and arrows of outrageous fortune, " \
-           "Or to take arms against a sea of troubles " \
-           "And by opposing end them. To die—to sleep,"
-
-    ssml = parseText(text)
-
+    ssml = "<speak>"
+    line1 = "To be, or not to be, that is the question:"
+    line2  = "Whether 'tis nobler in the mind to suffer"
+    line3  = "The slings and arrows of outrageous fortune,"
+    line4 = "Or to take arms against a sea of troubles"
+    line5 = "And by opposing end them. To die—to sleep,"
+    lines = [line1, line2, line3, line4, line5]
+    for i in lines:
+        ssml += parseText(i)
+        ssml += "<break time=\"200ms\" />"
+    ssml += "</speak>"
+    print(ssml)
     input_text = texttospeech.types.SynthesisInput(ssml=ssml)
 
     # Note: the voice can also be specified by name.
@@ -46,5 +57,6 @@ def synthesize_ssml():
         out.write(response.audio_content)
         print('Audio content written to file "output.mp3"')
 
+    playsound("output.mp3")
 
-playsound("output.mp3")
+synthesize_ssml()
